0ff12e0723b4713c09bb4ea86735b05d342aec88
Kamil Bakierzynski <bakierzynskikamil@gmail.com>
2020-03-18 19:20:24 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:zostawpoddrzwiami/screens/current_request.dart';
-=======
 import 'package:provider/provider.dart';
 import 'package:zostawpoddrzwiami/models/request_model.dart';
 import 'package:zostawpoddrzwiami/models/user_model.dart';
->>>>>>> a912c29718f5803e38785e4f1685bae7e1ef8c2c
 import 'package:zostawpoddrzwiami/screens/details_screen.dart';
-import 'package:zostawpoddrzwiami/screens/home_screen.dart';
 import 'package:zostawpoddrzwiami/services/auth_service.dart';
 import 'package:zostawpoddrzwiami/services/database_service.dart';
 import 'package:zostawpoddrzwiami/wrappers/auth_wrapper.dart';
