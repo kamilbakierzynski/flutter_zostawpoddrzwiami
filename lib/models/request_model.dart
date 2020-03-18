8a26d4f3ef9f97e83f6f0761687c2bfc79943aab
braniaq <kuba.branicki@gmail.com>
2020-03-18 15:46:36 +0100

    data models

---
+++ lib/models/request_model.dart
@@ -0,0 +1,8 @@
+class UserRequest{
+  final String name;
+  final String time;
+  final String price;
+  final String requestId;
+  final List<dynamic> request;
+  UserRequest({this.name,this.time, this.price, this.requestId, this.request});
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,10 @@
+class User
+{ final String uid;
+  User({this.uid});
+}
+class UserData
+{
+  final String uid;
+  final String name;
+  UserData( {this.uid, this.name});
+}
